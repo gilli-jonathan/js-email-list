a20c8e04cd3c4953c042cedb17ba5529ecc0486c
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-23 14:30:19 +0200

    work in progress

--- main.js
+++ main.js
@@ -14,5 +14,18 @@ const daddyEl = document.getElementById("mail-daddy");
 axios
   .get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((banane) => {
-    console.log(banane.data); // i dati ricevuti dal server
+    console.log(banane.data);
+    console.log(banane.data.response);
   });
+
+let lista = "";
+
+function create_li(para) {
+  for (let index = 0; index < 10; index++) {
+    const cosine = index;
+    console.log(cosine);
+    lista += `<li>${para.response}</li>`;
+  }
+
+  daddyEl.innerHTML = lista;
+}
